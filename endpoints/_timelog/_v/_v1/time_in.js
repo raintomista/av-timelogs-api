fe34b8e67bf3c3297ccbaa5cbca7727a02e0d254
Rain Tomista <rftomista@gmail.com>
2017-06-18 02:25:07 +0800

    Handle time in/time out API multiple requests

--- endpoints/_timelog/_v/_v1/time_in.js
+++ endpoints/_timelog/_v/_v1/time_in.js
@@ -1,34 +1,45 @@
 const moment = require('moment');
 const vars = require('../../../../vars');
 const Timelog = require('../../../../models/timelog');
+const User = require('../../../../models/user');
+
 
 module.exports = function(req, res, next){
+    const query = {username: req.params.username, timedIn: false};
+    const update = {timedIn: true};
+
     let newObject = {
         username: req.params.username,
         timeIn: moment().utc(),
         timeOut: null
     }
 
-    let newTimelog = new Timelog(newObject);
+    User.findOneAndUpdate(query, update, function(err, result){
+        if(result !== null){
+            let newTimelog = new Timelog(newObject);
 
-    newTimelog.save(function(err, timelog){
-        if(!err){
-            res.send(200, {
-                code: vars.CODE_SUCCESS,
-                msg: "Successfully timed in",
-                data: timelog
-            });
+            newTimelog.save(function(err, timelog){
+                if(!err){
+                    res.send(200, {
+                        code: vars.CODE_SUCCESS,
+                        msg: "Successfully timed in",
+                        data: timelog
+                    });
+                }
+                else{
+                    res.send(500, {
+                        code: vars.CODE_SUCCESS_ERROR,
+                        msg: vars.CODE_SUCCESS_ERROR,
+                        err: err
+                    });
+                }
+            })
         }
         else{
-            res.send(500, {
-                code: vars.CODE_SUCCESS_ERROR,
-                msg: vars.CODE_SUCCESS_ERROR,
-                err: err
+            res.send(400, {
+                code: vars.CODE_BAD_REQUEST,
+                msg: vars.CODE_BAD_REQUEST
             });
         }
-    })
-
-
-
-
+    });
 }
--- endpoints/_timelog/_v/_v1/time_out.js
+++ endpoints/_timelog/_v/_v1/time_out.js
@@ -3,7 +3,7 @@ const vars = require('../../../../vars');
 const Timelog = require('../../../../models/timelog');
 const User = require('../../../../models/user');
 module.exports = function(req, res, next){
-    const query = { username: req.params.username };
+    let query = { username: req.params.username };
     const update = { timeOut: moment().utc() };
     const options = { new: false, sort: {'timeIn': -1}};
     
@@ -18,18 +18,24 @@ module.exports = function(req, res, next){
 
                         if(total === 0) total = moment.duration(end.diff(start)).asHours();
                         else total = total + moment.duration(end.diff(start)).asHours();
-
-                        console.log(total);
                     });
 
-                    User.findOneAndUpdate(query, {totalHours: total}, function(err, result){
-                        if(!err){
+                    query = { username: req.params.username, timedIn: true };
+                    User.findOneAndUpdate(query, {totalHours: total, timedIn: false}, function(err, result){
+                        if(result == null){
+                            res.send(400, {
+                                code: vars.CODE_BAD_REQUEST,
+                                msg: vars.CODE_BAD_REQUEST,
+                                err: err
+                            });
+                        }
+                        else if(!err){
                             res.send(200, {
                                 code: vars.CODE_SUCCESS,
                                 msg: "Successfully timed out",
                             });
                         }
-                        else{
+                        else if(err){
                             res.send(500, {
                                 code: vars.CODE_SUCCESS_ERROR,
                                 msg: vars.CODE_SUCCESS_ERROR,
