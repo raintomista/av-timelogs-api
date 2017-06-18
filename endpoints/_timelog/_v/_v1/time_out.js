e657cb470e2e9cab366f5b51627bb9ba69821578
Rain Tomista <rftomista@gmail.com>
2017-06-18 10:27:11 +0800

    Compute total hours after time out

--- endpoints/_timelog/_v/_v1/time_out.js
+++ endpoints/_timelog/_v/_v1/time_out.js
@@ -1,21 +1,50 @@
 const moment = require('moment');
 const vars = require('../../../../vars');
 const Timelog = require('../../../../models/timelog');
-
-
+const User = require('../../../../models/user');
 module.exports = function(req, res, next){
     const query = { username: req.params.username };
     const update = { timeOut: moment().utc() };
     const options = { new: false, sort: {'timeIn': -1}};
     
-    Timelog.findOneAndUpdate(query, update, options, function(err, result){
-
-        console.log(result);
+    Timelog.findOneAndUpdate(query, update, options, function(err){
         if(!err){
-            res.send(200, {
-                code: vars.CODE_SUCCESS,
-                msg: "Successfully timed out",
-                data: result
+            Timelog.find(query, function(err, result){
+                if(!err){
+                    let total = 0;
+                    result.forEach(function(e){
+                        const start = e.timeIn;
+                        const end = moment(e.timeOut);
+
+                        if(total === 0) total = moment.duration(end.diff(start)).asHours();
+                        else total = total + moment.duration(end.diff(start)).asHours();
+
+                        console.log(total);
+                    });
+
+                    User.findOneAndUpdate(query, {totalHours: total}, function(err, result){
+                        if(!err){
+                            res.send(200, {
+                                code: vars.CODE_SUCCESS,
+                                msg: "Successfully timed out",
+                            });
+                        }
+                        else{
+                            res.send(500, {
+                                code: vars.CODE_SUCCESS_ERROR,
+                                msg: vars.CODE_SUCCESS_ERROR,
+                                err: err
+                            });
+                        }
+                    });
+                }
+                else{
+                    res.send(500, {
+                        code: vars.CODE_SUCCESS_ERROR,
+                        msg: vars.CODE_SUCCESS_ERROR,
+                        err: err
+                    });
+                }
             });
         }
         else{
@@ -26,5 +55,6 @@ module.exports = function(req, res, next){
             });
         }
     });
+
     
 }
