2b8482bb0bbfc65ded01b4dccb62f9261f6f1a96
Rain Tomista <rftomista@gmail.com>
2017-06-19 18:12:37 +0800

    Clean user.js model code

--- models/user.js
+++ models/user.js
@@ -12,35 +12,3 @@ var userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
-
-
-// module.exports.createUser = function(user, callback){
-// 	User.create(user, callback);
-// }
-
-// module.exports.retrieveUsers = function(callback, limit){
-// 	User.find(callback).limit(limit);
-// }
-
-// module.exports.retrieveUserById = function(id, callback){
-// 	User.findById(id, callback);
-// }
-
-// module.exports.updateUser = function(id, user, options, callback){
-// 	var query = {_id: id};
-// 	var update = {
-// 		username: user.username,
-// 		password: user.password,
-// 		name: user.name,
-// 		email: user.email,
-// 		contactNumber: user.contactNumber,
-// 		timeLogs: user.timeLogs,
-// 		totalHours: user.totalHours
-// 	}
-// 	User.findOneAndUpdate(query, update, options, callback);
-// }
-
-// module.exports.deleteUser = function(id, callback){
-// 	var query = {_id: id};
-// 	User.remove(query, callback);
-// }
