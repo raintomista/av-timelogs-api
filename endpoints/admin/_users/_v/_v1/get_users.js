c95d8ec58a17dbea7780c8d7fd513f113ae83509
Pauline Reyes <Pauline Reyes>
2017-06-20 11:26:09 +0800

    deploy

--- endpoints/admin/_users/_v/_v1/get_users.js
+++ endpoints/admin/_users/_v/_v1/get_users.js
@@ -6,20 +6,21 @@ const User = require('../../../../../models/user');
 module.exports = function(req, res, next){
     User.find({}, function(err, result){
         if(!err){
-            if(result.length > 0){
-                res.send(200, {
-                    code: vars.CODE_SUCCESS,
-                    msg: "Successfully fetched",
-                    data: result
-                });
-            }
-            else{
-                res.send(200, {
-                    code: vars.CODE_SUCCESS,
-                    msg: "No data fetched",
-                    data: result
-                });
-            }
+
+            res.send(200,{code: "success", data: result});
+            // if(result.length > 0){
+            //     res.send(200, {
+            //         code: vars.CODE_SUCCESS,
+            //         msg: "Successfully fetched",
+            //         data: result
+            //     });
+            // }
+            // else{
+            //     res.send(200, {
+            //         code: vars.CODE_SUCCESS,
+            //         msg: "No data fetched"
+            //     });
+            // }
         }
         else{
             res.send(500, {
--- endpoints/admin/_users/routes.js
+++ endpoints/admin/_users/routes.js
@@ -6,8 +6,8 @@ const get_user_by_username = require('./_v/_v1/get_user_by_username');
 const get_users_by_status = require('./_v/_v1/get_users_by_status');
 const create_user_v1 = require('./_v/_v1/create_user');
 
-api.get({path: '/users/:username'}, core.verifytoken, get_user_by_username);
-api.post({path: '/users/create'}, core.verifytoken,create_user_v1);
 api.get({path: '/users/all'}, core.verifytoken, get_user_v1);
+api.get({path: '/users/:username'}, core.verifytoken, get_user_by_username);
 api.get({path: '/users/status/:status'}, core.verifytoken, get_users_by_status);
+api.post({path: '/users/create'}, core.verifytoken, create_user_v1);
 
