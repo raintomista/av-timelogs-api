290d535aa4cab0d064f943e57aecf0b0f8c047e7
Rain Tomista <rftomista@gmail.com>
2017-06-30 11:11:24 +0800

    Implement get network status

---
+++ endpoints/admin/_network/_v/_v1/get_network_status.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const vars = require('../../../../../vars');
+const Network = require('../../../../../models/network');
+
+module.exports = function(req, res, next){
+    Network.find({ip_address: req.params.ip}, function(err, result){
+        if(!err){
+            res.send(200, {
+                code: vars.CODE_SUCCESS,
+                msg: "Successfully fetched network",
+                data: result
+            });
+        } else {
+            res.send(500, {
+                code: vars.CODE_SERVER_ERROR,
+                msg: vars.CODE_SERVER_ERROR,
+                err: err
+            });
+        }
+    });
+}
--- endpoints/admin/_network/routes.js
+++ endpoints/admin/_network/routes.js
@@ -2,12 +2,14 @@ const api = module.parent.exports.api;
 const core = require('../../../services/core-service');
 
 const get_networks_v1 = require('./_v/_v1/get_networks');
+const get_network_status_v1 = require('./_v/_v1/get_networks');
 const add_network_v1 = require('./_v/_v1/add_network');
 const remove_valid_v1 = require('./_v/_v1/remove_valid');
 const set_valid_v1 = require('./_v/_v1/set_valid');
 const delete_network_v1 = require('./_v/_v1/delete_network');
 
 api.get({path: '/timelogs/host'}, core.verifytoken, get_networks_v1);
+api.post({path: '/timelogs/host/status'}, core.verifytoken, get_network_status_v1);
 api.post({path: '/timelogs/add_host'}, core.verifytoken, add_network_v1);
 api.put({path: '/timelogs/remove_host/:_id'}, core.verifytoken, remove_valid_v1);
 api.put({path: '/timelogs/set_valid/:_id'}, core.verifytoken, set_valid_v1);
