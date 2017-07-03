350ad66d9f85f821b77b4fb86d076d93db48ad5b
Rain Tomista <rftomista@gmail.com>
2017-07-04 02:52:13 +0800

    Implement CRON

--- services/scheduled_email.js
+++ services/scheduled_email.js
@@ -2,8 +2,26 @@ const moment = require('moment');
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const sendgrid = require('./sendgrid');
+const cron = require('node-schedule');
 
 
+cron.scheduleJob({hour: 12, minute: 00, dayOfWeek: [1,2,3,4,5]}, function(){
+    console.log('NOON MAILER');
+    timeInAlert();
+});
+
+cron.scheduleJob({hour: 19, minute: 00, dayOfWeek: [1,2,3,4,5]}, function(){
+    console.log('NIGHT MAILER');
+    endOfTheDayAlert();
+});
+
+cron.scheduleJob({hour: 2}, function(){
+    console.log('Test2');
+});
+
+cron.scheduleJob({second: 20}, function(){
+    console.log('Test');
+});
 // setInterval(function(){
 //     const currentTime = moment().utcOffset('+08:00');
 //     timeInAlert(currentTime);
@@ -13,23 +31,23 @@ const sendgrid = require('./sendgrid');
 
 
 // Alerts
-timeInAlert = function(currentTime){
-    const noonTime = moment().startOf('day').add(12, 'hours');
-    console.log(`${noonTime.format('HH:mm:ss')} ${currentTime.format('HH:mm:ss')} -- TIME IN ${noonTime.format('HH:mm:ss') === currentTime.format('HH:mm:ss')}`);
-    if(noonTime.format('HH:mm:ss') === currentTime.format('HH:mm:ss')){
+timeInAlert = function(){
+    // const noonTime = moment().startOf('day').add(12, 'hours');
+    // console.log(`${noonTime.format('HH:mm:ss')} ${currentTime.format('HH:mm:ss')} -- TIME IN ${noonTime.format('HH:mm:ss') === currentTime.format('HH:mm:ss')}`);
+    // if(noonTime.format('HH:mm:ss') === currentTime.format('HH:mm:ss')){
         User.find({status: 0}, {email: 1, name: 1}, function(err, results){
             sendgrid.personalizeEmailByRecipient(results);
         });
-    }
+    // }
 }
 
-endOfTheDayAlert = function(currentTime){
+endOfTheDayAlert = function(){
     const start = moment().startOf('day').add(9, 'hours');
     const end = moment().startOf('day').add(19, 'hours');
-    const endOfTheDay = moment().startOf('day').add(19, 'hours');
-    console.log(`${endOfTheDay.format('HH:mm:ss')} ${currentTime.format('HH:mm:ss')} -- END OF THE DAY ALERT ${endOfTheDay.format('HH:mm:ss') === currentTime.format('HH:mm:ss')}`);
+    // const endOfTheDay = moment().startOf('day').add(19, 'hours');
+    // console.log(`${endOfTheDay.format('HH:mm:ss')} ${currentTime.format('HH:mm:ss')} -- END OF THE DAY ALERT ${endOfTheDay.format('HH:mm:ss') === currentTime.format('HH:mm:ss')}`);
 
-    if(end.format('HH:mm:ss') === currentTime.format('HH:mm:ss')){
+    // if(end.format('HH:mm:ss') === currentTime.format('HH:mm:ss')){
         User.find()
         .populate('_timelog')
         .exec(function(err, results){
@@ -47,6 +65,6 @@ endOfTheDayAlert = function(currentTime){
                 sendgrid.emailWithList(absentUsers);
             }
         });
-    }
+    // }
     
 }
