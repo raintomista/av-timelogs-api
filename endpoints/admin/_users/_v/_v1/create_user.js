4024aee4a4783829f20b64b396d6b1458321ed62
Rain Tomista <rftomista@gmail.com>
2017-07-05 18:28:49 +0800

    Enable backend uploading

--- endpoints/admin/_users/_v/_v1/create_user.js
+++ endpoints/admin/_users/_v/_v1/create_user.js
@@ -8,7 +8,9 @@ const saltRounds = 10;
 var salt = bcrypt.genSaltSync(saltRounds);
 
 module.exports = function(req,res,next){
-    // cloudinary.upload(req.params.data.imgUrl, function(result){
+    cloudinary.upload(req.params.data.imgUrl, function(result){
+
+    console.log(result);
      var data = {
                 username: req.params.data.username,
                 password: bcrypt.hashSync(req.params.data.password,salt),
@@ -17,18 +19,22 @@ module.exports = function(req,res,next){
                 contactNumber: req.params.data.contactNumber,
                 totalHours: null,
                 status: 0,
-                imgUrl: req.params.data.imgUrl,
+                imgUrl: result.secure_url,
                 _timelog: null
             };
 
     
         //Username and Email Availability
     User.find({$or: [{username: req.params.data.username},{email: req.params.data.email}]}).exec(function(err,results){
+        console.log(err);
+        console.log(results);
+        
         if(!err){
             if(results.length > 0){
                 res.send(400,{code: vars.CODE_BAD_REQUEST , message:"Username/Email already exists"});
             } else {
                 User.create(data, function(test){
+                    console.log(test);
                     res.send(200, {
                         code: vars.CODE_SUCCESS, 
                         message:"User has been created",
@@ -40,5 +46,5 @@ module.exports = function(req,res,next){
             res.send(500,{code: "Error", message: vars.MSG_SERVER_ERROR});
         }  
      })
-//   })
+  })
 };
