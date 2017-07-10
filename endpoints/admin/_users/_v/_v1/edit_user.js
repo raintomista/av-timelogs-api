84c222ca942ab43360978ec0d9d6a7a6c27aed3c
Rain Tomista <rftomista@gmail.com>
2017-07-10 17:46:49 +0800

    Implement Edit User

---
+++ endpoints/admin/_users/_v/_v1/edit_user.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const vars = require('../../../../../vars');
+const User = require('../../../../../models/user');
+const cloudinary = require('../../../../../services/cloudinary');
+
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+var salt = bcrypt.genSaltSync(saltRounds);
+
+module.exports = function(req,res,next){
+    User.findOne({username: req.params.data.username}, function(err, user){
+        if(!err){
+            cloudinary.upload(req.params.data.imgUrl, function(response){
+                let newData = {
+                    username: req.params.data.username,
+                    password: bcrypt.hashSync(req.params.data.password, salt),
+                    name: req.params.data.name,
+                    email: req.params.data.email,
+                    contactNumber: req.params.data.contactNumber,
+                    imgUrl: response.secure_url
+                }
+                User.update({username: req.params.data.username}, newData, function(err, response){
+                    if(!err){
+                        res.send(200, {
+                            code: vars.CODE_SUCCESS, 
+                            message: `Your account has been updated!`,
+                        });
+                    }
+                    else{
+                        res.send(500, {
+                            code: vars.CODE_SERVER_ERROR, 
+                            message: vars.CODE_SERVER_ERROR,
+                            err: err
+                        });
+                    }
+                });
+            });
+        }
+        else{
+            res.send(500, {
+                code: vars.CODE_SERVER_ERROR, 
+                message: vars.CODE_SERVER_ERROR,
+                err: err
+            });
+        }
+    });
+};
+
+
+
+    
+    //                     console.log(result);
+    //  var data = {
+    //             username: req.params.data.username,
+    //             password: bcrypt.hashSync(req.params.data.password,salt),
+    //             name: req.params.data.name,
+    //             email: req.params.data.email,
+    //             contactNumber: req.params.data.contactNumber,
+    //             totalHours: null,
+    //             status: 0,
+    //             imgUrl: result.secure_url,
+    //             _timelog: null
+    // };
+//   });
