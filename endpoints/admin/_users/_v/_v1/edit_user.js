e6f1e44cc893b0a13ed96928eed2d2d97a55f05e
Rain Tomista <rftomista@gmail.com>
2017-07-10 18:43:40 +0800

    Edit User Error Handling

--- endpoints/admin/_users/_v/_v1/edit_user.js
+++ endpoints/admin/_users/_v/_v1/edit_user.js
@@ -10,15 +10,40 @@ var salt = bcrypt.genSaltSync(saltRounds);
 module.exports = function(req,res,next){
     User.findOne({username: req.params.data.username}, function(err, user){
         if(!err){
-            cloudinary.upload(req.params.data.imgUrl, function(response){
-                let newData = {
-                    username: req.params.data.username,
-                    password: bcrypt.hashSync(req.params.data.password, salt),
-                    name: req.params.data.name,
-                    email: req.params.data.email,
-                    contactNumber: req.params.data.contactNumber,
-                    imgUrl: response.secure_url
-                }
+            let newData = {};
+            newData.username = req.params.data.username;
+            newData.name = req.params.data.name;
+            newData.email = req.params.data.email;
+            newData.contactNumber = req.params.data.contactNumber;
+            if(req.params.data.password !== null){
+                console.log("new pass");
+                newData.password = bcrypt.hashSync(req.params.data.password, salt);
+            }
+            if(req.params.data.imgUrl !== null){
+                console.log("new image");
+                
+                cloudinary.upload(req.params.data.imgUrl, function(response){
+                    newData.imgUrl = response.secure_url;             
+                    User.update({username: req.params.data.username}, newData, function(err, response){
+                        if(!err){
+                            res.send(200, {
+                                code: vars.CODE_SUCCESS, 
+                                message: `Your account has been updated!`,
+                            });
+                        }
+                        else{
+                            res.send(500, {
+                                code: vars.CODE_SERVER_ERROR, 
+                                message: vars.CODE_SERVER_ERROR,
+                                err: err
+                            });
+                        }
+                    });
+                });
+            }
+            else{
+                console.log("no new image");
+                       
                 User.update({username: req.params.data.username}, newData, function(err, response){
                     if(!err){
                         res.send(200, {
@@ -34,7 +59,7 @@ module.exports = function(req,res,next){
                         });
                     }
                 });
-            });
+            }
         }
         else{
             res.send(500, {
