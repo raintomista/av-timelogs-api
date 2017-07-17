a2780e19472e6bf81cdfb31e2e827a8db301af5a
Rain Tomista <rftomista@gmail.com>
2017-07-17 21:23:13 +0800

    Edit User Fix

---
+++ endpoints/admin/_users/_v/_v2/edit_user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const vars = require('../../../../../vars');
+const User = require('../../../../../models/user');
+const cloudinary = require('../../../../../services/cloudinary');
+
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+var salt = bcrypt.genSaltSync(saltRounds);
+
+module.exports = function(req,res,next){
+    User.findOne({username: req.params.data.username}, function(err, user){
+        if(!err){
+            cloudinary.upload(req.params.data.imgUrl, function(response){
+                if(response.secure_url){
+                    user.imgUrl = response.secure_url;
+                }
+                if(req.params.data.newUsername){
+                    user.username = req.params.data.newUsername;
+                }
+                if(req.params.data.password){
+                    user.password = bcrypt.hashSync(req.params.data.password, salt);
+                }
+
+                user.firstName = req.params.data.firstName;
+                user.lastName = req.params.data.lastName;
+                user.email = req.params.data.email;
+                user.contactNumber = req.params.data.contactNumber;
+
+                user.save(function(err, result){
+                    if(!err){
+                        res.send(200, {
+                            code: vars.CODE_SUCCESS, 
+                            message: 'Successfully updated account.',
+                            data: result
+                        });
+                    }
+                    else{
+                        res.send(500, {
+                            code: vars.CODE_SERVER_ERROR, 
+                            message: vars.CODE_SERVER_ERROR, 
+                            err: err
+                        });   
+                    }
+                });
+
+            });
+        }
+        else{
+            res.send(500, {
+                code: vars.CODE_SERVER_ERROR, 
+                message: vars.CODE_SERVER_ERROR, 
+                err: err
+            });    
+        }
+    });
+}
+
+
